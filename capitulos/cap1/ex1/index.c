aa7079cb0c0a65fcb9940bfa12d1001bcdae1ca0
williamelias <williameliasalves@gmail.com>
2023-10-12 17:23:06 -0300

    refactor: add new comments and code.

--- capitulos/cap1/ex1/index.c
+++ capitulos/cap1/ex1/index.c
@@ -1,6 +1,15 @@
 #include <stdio.h>
 
-void main()
+/*/
+Esse é um comentário
+
+*/
+int main(int argc, char const *argv[])
 {
-  printf("Hello meu caro !! \n");
+  printf(
+    "Hello, \"World\"!\tMy"
+    "name is william\n"
+  );
+  printf("amor\vvoce\n");
+  return 0;
 }
--- capitulos/cap2/numberE/index.c
+++ capitulos/cap2/numberE/index.c
@@ -1,15 +1,24 @@
 #include <stdio.h>
 
+/*
+*Os tipos de dados básicos na linguagem C são float,double, int,char
+*Os tamanhos de espaço de cada uma é
+*- float: 4 bytes
+*- double: 8 bytes
+*- int: 4 bytes
+*- char: 1 byte
+*/
+
 int main(int argc, char const *argv[])
 {
-    
+    const float PI = 3.14; //declaração e atribuição de variável
     float kilos = 1.0E3;
     double gramas = 1.0e6;
-    double toneladas;
+    double toneladas; // declaração de variável
 
     printf("insira a qunatidade em toneladas:");scanf("%lf",&toneladas);
 
-    printf("a quantidade %.0lf em kilos é %e e em gramas é %E",toneladas,toneladas*kilos,\
+    printf("a quantidade %.0lf em kilos é %e e em gramas é %E\n",toneladas,toneladas*kilos,\
     toneladas*gramas);
 
     return 0;
